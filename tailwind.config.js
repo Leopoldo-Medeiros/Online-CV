8f17294fca0094f554adcb38f5e2433f01577f31
leopoldomedeiros <leopldof.medeiros@gmail.com>
2025-12-16 22:40:07 +0000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2563eb',
+        secondary: '#1e40af',
+        dark: '#1f2937',
+        light: '#f9fafb',
+      },
+      textAlign: {
+        'justify': 'justify',
+      },
+    },
+  },
+  plugins: [],
+}
